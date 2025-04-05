91a5d8e499688008174eb51c69b3aec39a89a831
SaiPh3r <saianand750@gmail.com>
2025-04-05 19:39:29 +0530

    nodemailer setup

---
+++ utility/mail.ts
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 465,
+    secure: true, 
+    auth: {
+      user: "maddison53@ethereal.email",
+      pass: "jn7jnAPss4f63QBp6D",
+    },
+  });
+
+
+  async function main(email: string,emailType: string,userId: string) {
+    
+    const info = await transporter.sendMail({
+      from: 'sai@gmail.ai', 
+      to: email,
+      subject: emailType==="verify"?"verify":"Reset Password", 
+      html: "<b>Hello world?</b>", 
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    
+  }
+  
+  main("vishal@gmail.ai", "verify", "123abc").catch(console.error);
