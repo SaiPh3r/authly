759f1a2d9c2d59ee075266f4c8e118b5e75c8ee1
SaiPh3r <saianand750@gmail.com>
2025-04-06 18:34:52 +0530

    frontend basic start

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const page = () => {
+const SignUpPage = () => {
+  const[user,setUser] = useState({
+    user:"",
+    email:"",
+    password:"",
+  })
+  const[loader,setLoader] = useState(false)
+
+  const onSignUp = async ()=>{
+    try {
+      setLoader(true)
+      const response = await fetch("api/signUp",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(user)
+        
+      })
+      const responseData = await response.json()
+      console.log("woah user signed up!!",responseData)
+
+
+      
+    } catch (error:any) {
+      console.log(error.message)
+      console.log("signup failed")
+
+
+      
+      
+    }
+
+  }
+
+
+
+
   return (
     <div>signup</div>
   )
 }
 
-export default page
+export default SignUpPage
