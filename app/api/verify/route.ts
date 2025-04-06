a46a9255c218e06ca70a02267e388b854e639c59
SaiPh3r <saianand750@gmail.com>
2025-04-06 12:30:58 +0530

    corrected verify

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -7,24 +7,37 @@ import {main} from "@/utility/mail"
 connect()
 
 export async function POST(request:NextRequest){
-   const reqBody = await request.json()
-   const{hashedToken} = reqBody
-   console.log(hashedToken)
+    try {
+        const reqBody = await request.json()
+        const{token} = reqBody
+        console.log(token)
+     
+        const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now() } })
+     
+        if(!user){
+         return NextResponse.json({
+             error:"Invalid or expired token"},
+             {status:400})
+     
+        }
+        console.log(user)
+        user.isVerified= true
+        user.verifyToken = undefined
+        user.verifyTokenExpiry = undefined
+     
+        await user.save()
+        return NextResponse.json({
+         message: "Email successfully verified",
+         success: true
+       })
+        
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({error:"Something went wrong"},{status:500})
+        
+    }
 
-   const user = await User.findOne({ verifyToken: hashedToken, verifyTokenExpiry: { $gt: Date.now() } })
-
-   if(!user){
-    return NextResponse.json({
-        error:"Invalid or expired token"},
-        {status:400})
-
-   }
-   console.log(user)
-   user. isVerified= true
-   user. verifyToken = undefined
-   user.verifyTokenExpiry = undefined
-
-   await user.save()
 
     
 }
