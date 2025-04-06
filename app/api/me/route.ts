58c929e484625811b9323edf53a5de5148359513
SaiPh3r <saianand750@gmail.com>
2025-04-06 15:20:53 +0530

    me route setup

---
+++ app/api/me/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest,NextResponse } from "next/server";
+import {connect} from "@/db/dbConfig"
+import {User} from "@/models/userModels"
+import { getData } from "@/utility/getDataFromToken";
+
+connect()
+
+export async function POST(request:NextRequest){
+    const userId = await getData(request);
+    const user = await User.findOne({_id: userId }).select('-password')
+
+    if(!user){
+        return NextResponse.json({
+            error:"User not found"},
+        {status:400})
+    }
+
+    return NextResponse.json({
+        user,
+        success:true,
+    })
+
+}
